33aa4d66420b277c63cfd1d705ffdd2f888d0500
joynatalie <99263754+joynatalie@users.noreply.github.com>
2022-03-11 23:01:30 +0300

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-  int lw = 'a';
-  while (lw <= 'z')
-    {
-      if (lw == 'e' || lw == 'q')
-	{
-	  lw += 1;
-	}
-      else
-	{
-	  putchar(lw);
-	  lw += 1;
-	}
-    }
-  putchar('\n');
-  return (0);
+int lw = 'a';
+while (lw <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+lw += 1;
+}
+else
+{
+putchar(lw);
+lw += 1;
+}
+}
+putchar('\n');
+return (0);
 }
