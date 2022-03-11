1b9899748d8215098da983aa0ea75d00bffdeaec
joynatalie <99263754+joynatalie@users.noreply.github.com>
2022-03-12 02:18:03 +0300

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-  int num;
-  char letter;
-  for (num = 0; num < 10; num++)
-    putchar((num % 10) + '0');
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 
-  for (letter = 'a'; letter <= 'f'; letter++)
-    putchar(letter);
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
