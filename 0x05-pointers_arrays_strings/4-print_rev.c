4521eb7975b08a3c72838c39e6d5071aad09c24f
joynatalie <joynatalie1906@gmail.com>
2022-03-21 14:27:41 -0700

    rev string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,14 @@
 
 void print_rev(char *s)
 {
-int len = 0, index;
-while (s[index++])
-len++;
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+int i = 0;
+ while (*(s + i) != '\0')
+i += 1;
+i -= 1;
+while (i >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
 _putchar('\n');
 }
