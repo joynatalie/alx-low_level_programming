445e985c40851edd02fc65a136a87400a4345c24
joynatalie <99263754+joynatalie@users.noreply.github.com>
2022-04-12 00:58:34 +0300

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
-
 /**
  * struct dog - struture of a dog
  * @name: name of dog
@@ -15,10 +12,4 @@ typedef struct dog
 	char *owner;
 
 } ;
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif 
+ 
