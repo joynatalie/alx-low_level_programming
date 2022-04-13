546f5487c2a8292a53630b7d03f017e101fe8374
joynatalie <joynatalie1906@gmail.com>
2022-04-13 10:16:04 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name
+ *@name: string to print
+ *@f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
