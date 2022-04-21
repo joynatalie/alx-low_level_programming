ef3cd88ed97c3b413c5b12f399b6bfa38d32e7aa
joynatalie <99263754+joynatalie@users.noreply.github.com>
2022-04-21 23:01:35 +0300

    nodeend

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,13 @@
 #include "lists.h"
-he function fails - NULL.
+#include <string.h>
+
+/**
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
 list_t *add_node_end(list_t **head, const char *str)
